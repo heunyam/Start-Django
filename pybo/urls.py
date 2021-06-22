07aa8eae14e7fefd0cfbe59ca1878aa3ee1c5dc6
heunyam <tpwns072@gmail.com>
2021-06-22 23:07:16 +0900

    feat: pybo/urls.py 에 pybo namespace 추가

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import index, detail
 
+
+app_name = 'pybo'
+
 urlpatterns = [
     path('', index, name="index"),
     path('<int:question_id>', detail, name='detail')

37cb217945435268f2da2abcc3bb08a22710d70b
heunyam <tpwns072@gmail.com>
2021-06-22 23:02:33 +0900

    fix: get 함수는 해당 리소스가 없으면 에러를 raise 하기 때문에 대신 404를 리턴하도록 수정

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from pybo.models import Question
 
@@ -12,8 +12,8 @@ def index(request):
     return render(request, 'pybo/question_list.html', context)
 
 
-def detail(request, question_id ):
-    question = Question.objects.get(id=question_id)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     context = {
         'question': question
     }
